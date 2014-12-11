271de2c492b2e38b0e24da995138b1dffc45d58b
Austin Lyons <austinlyons@gmail.com>
2014-12-10 20:43:09 -0600

    LinkedList can be used to implement a stack

--- linked-list/js/stack.js
+++ linked-list/js/stack.js
@@ -1,1 +1,13 @@
+/* Use the LinkedList as a stack */
 var LinkedList = require('./linked-list');
+var stack = new LinkedList();
+stack.push('world');
+stack.push('hello');
+
+console.log(stack.toStr()); // hello->world
+console.log(stack); // { _head: { data: 'hello', next: { data: 'world', next: null } }, _length: 2 }
+
+console.log(stack.pop()); // pop the top of the stack: 'hello'
+console.log(stack.toStr()); // world
+console.log(stack); // { _head: { data: 'world', next: null }, _length: 1 }
+
